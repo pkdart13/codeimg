e8b67f60157aa095eb3b2bc6ba8872e8c0f53d4b
PRANAV K D <pkdart13@gmail.com>
2022-12-19 19:49:07 +0530

    Update TheSidebar.vue

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -113,7 +113,7 @@
                 >
                   <input type="file" class="sr-only" @change="handlePicture" />
                   <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="1280.000000pt" height="1280.000000pt" viewBox="0 0 1280.000000 1280.000000"
+ width="24" height="24" viewBox="0 0 1280.000000 1280.000000"
  preserveAspectRatio="xMidYMid meet">
 
 <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
